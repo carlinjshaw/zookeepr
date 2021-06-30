f6b8015496b5cd87dbc7ee915ff92a18a92d1b8c
carlinjshaw <carlinjshaw@gmail.com>
2021-06-30 18:31:49 -0400

    lots of routes connecting the css and js and other things

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
+
 const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -110,6 +113,22 @@ function filterByQuery(query, animalsArray) {
     }
     return true;
   }
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
